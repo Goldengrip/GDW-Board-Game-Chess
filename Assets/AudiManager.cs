b2a7aefb7d9a024a2419a7cb7665e5289dcfde77
Eljaca72 <cartersimmons72@gmail.com>
2023-10-16 13:01:44 -0400

    fixed audio

--- Assets/AudiManager.cs
+++ Assets/AudiManager.cs
@@ -3,9 +3,21 @@ using UnityEngine;
 
 public class AudiManager : MonoBehaviour
 {
+    public static AudiManager instance;
     // Start is called before the first frame update
     private void Awake()
     {
         DontDestroyOnLoad(this);
+    }
+    private void Start()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
     }
 }
